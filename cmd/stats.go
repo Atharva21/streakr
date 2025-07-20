5055fe486af5546d8bb5cc972980160372627d5b
Atharva21 <atharva.m.mulay@gmail.com>
2025-07-20 12:24:53 +0530

    add usage of stats cmd

--- cmd/stats.go
+++ cmd/stats.go
@@ -14,13 +14,12 @@ import (
 
 var statsCmd = &cobra.Command{
 	Use:   "stats",
-	Short: "Stats gives current streak, max streak",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Stats gives us info about one or all our habits",
+	Long: `To see current(running) streak and max streak for all habits:
+  streakr stats
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+To see habit wise monthly heatmap:
+  streak stats <habitname>`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
 			return tui.RenderOverallStats(cmd.Context())
